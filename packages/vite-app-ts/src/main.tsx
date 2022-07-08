28244027bbaec9a69e1ce3230efd5fb44b7fa798
Tiago Moutinho <tiagoagm@gmail.com>
2022-07-08 20:31:30 +0100

    fix routing issue

--- packages/vite-app-ts/src/main.tsx
+++ packages/vite-app-ts/src/main.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { Main } from '@1hive/1hive-ui';
 
 import { EthersModalConnector, useEthersContext } from 'eth-hooks/context';
@@ -16,12 +16,12 @@ import { truncateAddress } from './helpers';
 import { Add } from './components/modals';
 import React, { useState } from 'react';
 import { DownArrowIcon, UpArrowIcon } from './components/accordion';
-import { getNetworkNameByChainID } from './models/constants/networks';
 import { Modal, Popover } from 'antd';
 
 import './styles/app.less';
 import StreamsPack from './pages/streamspack';
 import MyStreams from './pages/mystreams';
+import { getNetworkNameByChainID } from './models/constants/networks';
 
 export enum RoutesPath {
   HOME = '/',
@@ -57,95 +57,99 @@ const MainApp = () => {
 
   return (
     <Main layout={false} scrollView={false}>
-      <main>
-        <header>
-          <div className="flex items-center gap-8">
-            <a href="/">
-              <h1 className="mb-0 text-2xl font-bold">Streaming Bee</h1>
-            </a>
-            <div className="relative flex items-center gap-6">
-              <div>
-                <button
-                  className="flex items-center px-3 py-2  font-semibold text-white bg-green-600 pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-green-500 gap-2"
-                  onClick={() => setIsAddModalVisible(!isAddModalVisible)}>
-                  <PlusOutlined />
-                  Add
-                </button>
+      <BrowserRouter>
+        <main>
+          <header>
+            <div className="flex items-center gap-8">
+              <a href="/">
+                <h1 className="mb-0 text-2xl font-bold">Streaming Bee</h1>
+              </a>
+              <div className="relative flex items-center gap-6">
+                <div>
+                  <button
+                    className="flex items-center px-3 py-2  font-semibold text-white bg-green-600 pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-green-500 gap-2"
+                    onClick={() => setIsAddModalVisible(!isAddModalVisible)}>
+                    <PlusOutlined />
+                    Add
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <Modal visible={isAddModalVisible} footer={null} onCancel={() => setIsAddModalVisible(false)}>
-            <p className="mb-4 text-base font-bold">Creating new Vesting</p>
-            <Add />
-          </Modal>
+            <Modal visible={isAddModalVisible} footer={null} onCancel={() => setIsAddModalVisible(false)}>
+              <p className="mb-4 text-base font-bold">Creating new Vesting</p>
+              <Add />
+            </Modal>
 
-          <div className="relative">
-            {ethersContext.account ? (
-              <div className="flex flex-col">
-                <Popover
-                  content={
-                    <>
-                      <p className="mb-4 text-base font-bold">Disconnect wallet</p>
-                      <button
-                        onClick={disconnect}
-                        className="px-3 py-2 font-semibold text-white bg-black pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-gray-500">
-                        Disconnect wallet
-                      </button>
-                    </>
-                  }
-                  trigger="click"
-                  visible={isWalletModal}
-                  onVisibleChange={() => setIsWalletModal(!isWalletModal)}>
-                  <div className="flex items-center justify-center text-right cursor-pointer gap-6 hover:text-gray-600">
-                    <div>
-                      <p className="font-bold text-black">
-                        Personal Wallet{' '}
-                        <p className="text-sm font-bold ">{getNetworkNameByChainID(ethersContext.chainId)}</p>
-                      </p>
-                      <p className="text-xs">{truncateAddress(ethersContext.account)}</p>
-                    </div>
-                    {!isWalletModal ? <DownArrowIcon /> : <UpArrowIcon />}
+            <div className="relative">
+              {ethersContext.account ? (
+                <div className="flex flex-wrap gap-4">
+                  <div className="flex items-center justify-center">
+                    <p className="px-2 py-1 text-xs font-bold text-white uppercase bg-amber-600 rounded-md">
+                      {getNetworkNameByChainID(ethersContext.chainId)}
+                    </p>
                   </div>
-                </Popover>
-              </div>
-            ) : (
-              <button
-                className="px-3 py-2 ml-8 font-semibold text-white bg-indigo-600 pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-indigo-500"
-                onClick={connect}>
-                Connect Wallet
-              </button>
-            )}
-          </div>
-        </header>
+                  <div className="flex flex-col">
+                    <Popover
+                      content={
+                        <>
+                          <p className="mb-4 text-base font-bold">Disconnect wallet</p>
+                          <button
+                            onClick={disconnect}
+                            className="px-3 py-2 font-semibold text-white bg-black pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-gray-500">
+                            Disconnect wallet
+                          </button>
+                        </>
+                      }
+                      trigger="click"
+                      visible={isWalletModal}
+                      onVisibleChange={() => setIsWalletModal(!isWalletModal)}>
+                      <div className="flex items-center justify-center text-right cursor-pointer gap-6 hover:text-gray-600">
+                        <div>
+                          <p className="font-bold text-black">Personal Wallet </p>
+                          <p className="text-sm">{truncateAddress(ethersContext.account)}</p>
+                        </div>
+                        {!isWalletModal ? <DownArrowIcon /> : <UpArrowIcon />}
+                      </div>
+                    </Popover>
+                  </div>
+                </div>
+              ) : (
+                <button
+                  className="px-3 py-2 ml-8 font-semibold text-white bg-indigo-600 pointer-events-auto rounded-md text-[0.8125rem] leading-5 hover:bg-indigo-500"
+                  onClick={connect}>
+                  Connect Wallet
+                </button>
+              )}
+            </div>
+          </header>
 
-        <nav>
-          <ul>
-            <li className="py-6 text-center">
-              <a href={RoutesPath.HOME} className="text-xl text-black">
-                <HomeOutlined />
-              </a>
-            </li>
-            <li className="py-6 text-center">
-              <a href={RoutesPath.MY_STREAMS} className="text-xl text-black">
-                <RetweetOutlined />
-              </a>
-            </li>
-            <li className="py-6 text-center">
-              <a href={RoutesPath.STREAMS_PACK} className="text-xl text-black">
-                <DollarOutlined />
-              </a>
-            </li>
-            <li className="py-6 text-center">
-              <a href={RoutesPath.FAQ} className="text-xl text-black">
-                <QuestionCircleOutlined />
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li className="py-6 text-center">
+                <Link to={RoutesPath.HOME} className="text-xl text-black">
+                  <HomeOutlined />
+                </Link>
+              </li>
+              <li className="py-6 text-center">
+                <Link to={RoutesPath.MY_STREAMS} className="text-xl text-black">
+                  <RetweetOutlined />
+                </Link>
+              </li>
+              <li className="py-6 text-center">
+                <Link to={RoutesPath.STREAMS_PACK} className="text-xl text-black">
+                  <DollarOutlined />
+                </Link>
+              </li>
+              <li className="py-6 text-center">
+                <Link to={RoutesPath.FAQ} className="text-xl text-black">
+                  <QuestionCircleOutlined />
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="content">
-          <BrowserRouter>
+          <div className="content">
             <Switch>
               <Route exact path={RoutesPath.HOME}>
                 <Home />
@@ -160,9 +164,9 @@ const MainApp = () => {
                 <FaqView />
               </Route>
             </Switch>
-          </BrowserRouter>
-        </div>
-      </main>
+          </div>
+        </main>
+      </BrowserRouter>
     </Main>
   );
 };
